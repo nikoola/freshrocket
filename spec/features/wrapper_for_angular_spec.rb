0d9155f9fc537c576c2d7437ef49fa2899ff82ad
lakesare <lakesare@gmail.com>
2016-03-03 01:14:35 +0500

    checked wrapped params

--- spec/features/wrapper_for_angular_spec.rb
+++ spec/features/wrapper_for_angular_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 
 
 
+# accepts simple args without model mentioned
 # https://github.com/rails/rails/issues/17216
 describe 'Angular: parameter wrapping', type: :request do
 
-	let(:user)         { FactoryGirl.create :user, abilities: [:cities] }
+	let(:user)         { FactoryGirl.create :user, abilities: [:cities, :users] }
 	let(:auth_headers) { user.create_new_auth_token }
 
-	it 'accepts simple args without model mentioned' do
+	it '/admin/cities' do
 
 		# we need to set additional headers for parameters wrapping to work
 		post '/admin/cities', { name: 'wow', areas_attributes: [
@@ -21,6 +22,26 @@ describe 'Angular: parameter wrapping', type: :request do
 		city = City.find(json_body[:id])
 
 		expect(city.areas.count).to eq(2)
+	end
+
+
+	it '/admin/users' do
+		client = FactoryGirl.create :user
+		password = 'hhhhhhhhhh'
+
+		#post works too
+		put "/admin/users/#{client.id}", {
+			password:              password,
+			password_confirmation: password
+		}.to_json, auth_headers.merge('ACCEPT' => 'application/json', 'CONTENT_TYPE' => 'application/json')
+
+		post '/auth/sign_in', {
+			email:    client.email,
+			password: password
+		}
+
+		expect_status 200 #401 if password was wrong
+		expect(headers).to include("access-token", "token-type", "client", "expiry", "uid")
 	end
 
 
