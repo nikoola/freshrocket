f777497f351f186a9f97ed8a66855b70490efda9
lakesare <lakesare@gmail.com>
2016-03-03 02:22:23 +0500

    _

--- spec/features/payment_spec.rb
+++ spec/features/payment_spec.rb
@@ -5,15 +5,19 @@ require 'capybara/rspec'
 
 Capybara.default_driver = :selenium
 
-
 describe 'Payment', type: [:feature, :request], js: true do #for some reason js: true gives us otherwise nonseen json response with factored things
 
-	let(:order){ FactoryGirl.create :order }
+
+	let(:user) { FactoryGirl.create :user }
+	let(:auth_headers) { user.create_new_auth_token }
+
+	let(:order){ FactoryGirl.create :order, user_id: user.id }
 
 	it 'citrus' do
-		# get '/client/payments/new', { order_id: order.id, amount: order.total_price }
-		# data = json_body[:citrus]
+		get '/client/payments/new', { order_id: order.id, amount: order.total_price }, auth_headers
+		data = json_body[:citrus]
 
+		expect_status 200
 
 		# visit '/products'
 
@@ -30,8 +34,7 @@ describe 'Payment', type: [:feature, :request], js: true do #for some reason js:
 
 
 		# page.evaluate_script("document.body.innerHTML += \"#{form_fields}\";")
-		# binding.pry
-		#works, now need to register as a client
+		# works, now need to register as a client
 	end
 
 
