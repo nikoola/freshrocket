8d8e1fb3023a81c5fff504d6ce527498951fa16d
lakesare <lakesare@gmail.com>
2016-03-08 21:02:52 +0500

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
 	ActionMailer::Base.smtp_settings = {
 		:user_name            => ENV['SENDGRID_USERNAME'],
-		:password             => ENV['SENDGRID_PASSWORD'].
+		:password             => ENV['SENDGRID_PASSWORD'],
 		:domain               => 'rivo.herokuapp.com',
 		:address              => 'smtp.sendgrid.net',
 		:port                 => 587,
