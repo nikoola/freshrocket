fa603e40cff7f0edeac4f451a8ef98fd69c0b9ef
lakesare <lakesare@gmail.com>
2016-03-12 22:23:34 +0500

    aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,8 @@ require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
 
+	puts "__________________________"
+	p ENV
 	config.fog_credentials = {
 		provider:              'AWS',                      
 		aws_access_key_id:     ENV['aws.access_key_id'],   
