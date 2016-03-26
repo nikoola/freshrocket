2969bf79bd447908093208d5f9211dd6a3c0cd99
lakesare <lakesare@gmail.com>
2016-03-26 07:22:30 +0500

    seeds

--- spec/factories/city.rb
+++ spec/factories/city.rb
@@ -1,6 +1,8 @@
+require_relative '../files/indian_cities'
+
 FactoryGirl.define do
 	factory :city do
-		sequence(:name) { |i| Faker::Address.city + '_' + i.to_s }
+		sequence(:name) { |i| INDIAN_CITIES[i] }
 	end
 
 	factory :same_city, parent: :city do
