1fedb95e8111ba33686238bd18059ada49a0f4fa
Dmitriy <dmitriy.gusev@duniceedge.com>
2016-08-22 14:50:42 +0300

    removing option_id from serializer

--- app/serializers/line_item_serializer.rb
+++ app/serializers/line_item_serializer.rb
@@ -1,7 +1,7 @@
 class LineItemSerializer < ActiveModel::Serializer
 	attributes :id, 
 		:order_id,
-		:amount, :fixed_price, :product_id, :option_id
+		:amount, :fixed_price, :product_id
 
 	has_one :product
 end
