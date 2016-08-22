94b367aba372818787440a534f41e219364452fb
Dmitriy <dmitriy.gusev@duniceedge.com>
2016-08-22 14:43:34 +0300

    debugging

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -16,6 +16,9 @@ module Admin
 
 		# GET /admin/orders/1
 		def show
+			puts 'debuggin!'
+			puts @order.to_json
+			puts params[:include]
 			render json: @order, include: params[:include]
 		end
 
