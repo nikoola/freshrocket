fe07a375af6ada7fcc402c3ebe1f16cbf0c13658
Juriaan <juriaan@Juriaans-Mac.local>
2016-09-08 19:11:21 +0200

    fix cityname

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,7 @@
 class CitiesController < ApplicationController
 
 	def index
-		@cities = City.find(:all, params.slice(:active, :name))
+		@cities = City.find(:all, :conditions => params.slice(:active, :name))
 		render json: @cities, include: params[:include]
 	end
 
