e17257e58a6bd5331e451720bd11ffd54711f9b6
Juriaan <juriaan@Juriaans-Mac.local>
2016-09-08 14:35:38 +0200

    bug fix

--- config/initializers/parallel_test.rb
+++ config/initializers/parallel_test.rb
@@ -1,19 +1,19 @@
-require "active_support/core_ext/module/aliasing"
-require "spring/application"
+# require "active_support/core_ext/module/aliasing"
+# require "spring/application"
 
-class Spring::Application
-  def connect_database_with_reconfigure_database
-    disconnect_database
-    reconfigure_database
-    connect_database_without_reconfigure_database
-  end
+# class Spring::Application
+#   def connect_database_with_reconfigure_database
+#     disconnect_database
+#     reconfigure_database
+#     connect_database_without_reconfigure_database
+#   end
 
-  alias_method_chain :connect_database, :reconfigure_database
+#   alias_method_chain :connect_database, :reconfigure_database
 
-  def reconfigure_database
-    if active_record_configured?
-      ActiveRecord::Base.configurations =
-        Rails.application.config.database_configuration
-    end
-  end
-end
+#   def reconfigure_database
+#     if active_record_configured?
+#       ActiveRecord::Base.configurations =
+#         Rails.application.config.database_configuration
+#     end
+#   end
+# end
